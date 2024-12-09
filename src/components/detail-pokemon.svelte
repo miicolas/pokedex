d55cd2459a7f0788a1db715b227597ffcc9eb7ad
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-12-09 20:38:06 +0100

    feat: detail-card components

---
+++ src/components/detail-pokemon.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+	type Data = {
+		typeDetail: string;
+		detail: string;
+	}
+
+
+	const { typeDetail, detail }:Data  = $$props;
+
+
+</script>
+
+<li class="text-gray-700">
+	<span class="font-medium">{typeDetail}:</span> {detail}
+</li>
