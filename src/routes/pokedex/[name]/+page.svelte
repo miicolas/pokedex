3263e8f18ffa955ce4199862e9e3f696ae5c470c
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-12-09 10:03:22 +0100

    fix: props

--- src/routes/pokedex/[name]/+page.svelte
+++ src/routes/pokedex/[name]/+page.svelte
@@ -1,18 +1,16 @@
 <script lang="ts">
-	export let data;
 
-	const { pokemon } = data;
+	let { data } = $props();
 
-	console.log(pokemon);
 </script>
 
 <h1 class="text-5xl font-bold text-gray-950">
-	{pokemon.name}
+	{data.pokemon.name}
 
-	{#each pokemon.abilities as { ability }}
+	{#each data.pokemon.abilities as { ability }}
 		<p class="font-medium">
 			<span class="font-bold">Ability:</span> {ability.name}
 		</p>
 	{/each}
-	<img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}  alt=""/>
+	<img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.pokemon.id}.png`}  alt=""/>
 </h1>
